5037910350a7634e78996479cd9b254ead18f81c
Cameron Gose <cgose001@gmail.com>
2013-04-26 02:37:31 -0700

    Apparently I didn't correctly push my collision detector that I wrote before and I no longer have the file that I wrote so now I will have to redo it. It will probably be better as a result

---
+++ dotdotdash/CollisionDetector.py
@@ -0,0 +1,22 @@
+from Sprite import *
+
+class CollisionDetector:
+	def __init__(self):
+		"""
+			Initialize object
+		"""
+		
+	def checkCollision(self,object1, object2):
+		"""
+			Checks if there is a collision between
+			object1 and object2
+		"""
+		
+	def collisionForecast(self,object1, objects):
+		"""
+			check if objects surrounding object1 
+			are close enough so that a collision is
+			likely to happen
+		"""
+	
+	
